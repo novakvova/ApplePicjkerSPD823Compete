6967edb077439d0f8c50fe7e021e9aa3f776ce28
Volodymyr Novak <novakvova@gmail.com>
2020-10-31 17:36:16 +0200

    compete

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    //Завжди викликається перед початком
+    public void Awake()
+    {
+        //Якщо значенян HighScore існує в PlayerPrefs, читаємо його
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        //Зберігаємо вище досягнуті HighScore в храніліще
+        PlayerPrefs.SetInt("HighScore", score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+
+        //Обновляємо HighScore в PlayerPrefs якщо необхідно
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+}
